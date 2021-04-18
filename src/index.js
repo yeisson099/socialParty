1b46d5b54c6a1fac3862b04fc8100b394e79861a
yeissonP <yeissonperez99@gmail.com>
2021-04-18 15:54:31 -0500

    prueba heroku

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const app = require('./app');
 require('./database');
 async function main() {
     await app.listen(app.get('port'));
-    console.log('server on port 8080');
+    console.log('server on port 8080 :)');
 }
 
 main();
