1ac598006211f2efda0d5df268e224e48bedd7c8
yeissonP <yeissonperez99@gmail.com>
2021-05-15 13:43:06 -0500

    new database

--- src/database/env.db.js
+++ src/database/env.db.js
@@ -1,8 +1,8 @@
-MONGODB_HOST     = "bdjtdiulbhsrmaq-mongodb.services.clever-cloud.com",
-MONGODB_DB       = "bdjtdiulbhsrmaq",
-MONGODB_USER     = "ukrrgpsdqvd3j6rwjcvg",
+MONGODB_HOST     = "buurnejvits6h8p-mongodb.services.clever-cloud.com",
+MONGODB_DB       = "buurnejvits6h8p",
+MONGODB_USER     = "urndudnf5igxujvybidy",
 MONGODB_PORT     = "27017",
-MONGODB_PASSWORD = "4jR7ncra2VZn5HnemJu6"
+MONGODB_PASSWORD = "dmdvD0kd1QHbhrpdQYkD"
 
 
 module.exports = {
